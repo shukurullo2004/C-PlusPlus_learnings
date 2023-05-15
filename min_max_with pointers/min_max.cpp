4d97c61fb2082a320db467d7c51f9abd9ff5d263
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-05-15 23:57:53 +0900

    dffg

---
+++ min_max_with pointers/min_max.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int getMax(int number[], int size) {
+    int max = number[0];
+    for (int i = 1; i < size; i++) {
+        if (number[i] > max) {
+            max = number[i];
+        }
+    }
+    return max;
+}
+
+
+
+int main() {
+    int num[4] = {900, 9, -67, 1000000};
+    int size = sizeof(num) / sizeof(num[0]);
+
+    cout << getMax(num,size) << endl;
+
+    return 0;
+}
