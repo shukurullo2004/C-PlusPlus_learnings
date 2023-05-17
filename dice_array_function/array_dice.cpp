4ef38b781260c9c6a6b990f1575148b6cb798a71
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-05-17 22:40:17 +0900

    array added

--- dice_array_function/array_dice.cpp
+++ dice_array_function/array_dice.cpp
@@ -1,3 +1,4 @@
+// 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -6,64 +7,46 @@ using namespace std;
 int _rand();
 
 int main(){
-    
     _rand();
     return 0;
 }
 
 int _rand(){
     srand((time(0)));
-    int one = 0;
-    int two = 0;
-    int three = 0;
-    int four = 0;
-    int five = 0;
-    int six = 0;
-
-    
+    int counts[6] = {0};
 
     for(int i = 0; i < 1000000; i++){
-        int num = rand()%6 +1;
-       
-        switch (num)
-        {
-        case 1:
-            one++;
-            break;
-        case 2:
-            two++;
-            break;
-        case 3:
-            three++;
-            break;
-        case 4:
-            four++;
-            break;
-        case 5:
-            five++;
-            break;
-        case 6:
-            six++;
-            break;
+        int num = rand() % 6 + 1;
+        switch (num) {
+            case 1:
+                counts[0]++;
+                break;
+            case 2:
+                counts[1]++;
+                break;
+            case 3:
+                counts[2]++;
+                break;
+            case 4:
+                counts[3]++;
+                break;
+            case 5:
+                counts[4]++;
+                break;
+            case 6:
+                counts[5]++;
+                break;
         }
     }
-    double percentage1 = (double)one/1000000 *100 ;
-    double percentage2 = (double)two/1000000 *100 ;
-    double percentage3 = (double)three/1000000 *100 ;
-    double percentage4 = (double)four/1000000 *100 ;
-    double percentage5 = (double)five/1000000 *100 ;
-    double percentage6 = (double)six/1000000 *100 ;
 
-    double sum = percentage1+ percentage2+percentage3+percentage4+percentage5+percentage6;
+    double percentages[6];
 
-    
-    cout <<"1: "<<  one<<", " << percentage1 << " %" <<endl;
-    cout <<"2: "<<  two<<", " << percentage2 << " %" << endl;
-    cout <<"3: "<<  three<<", " << percentage3 << " %" << endl;
-    cout <<"4: "<<  four<< ", " << percentage4 << " %" <<endl;
-    cout <<"5: "<<  five<< ", " << percentage5 << " %" <<endl;
-    cout <<"6: "<<  six<< ", " << percentage6 << " %" <<endl;
+    for(int i = 0; i < 6; i++){
+        percentages[i] = (double)counts[i] / 1000000 * 100;
+    }
 
-    cout << sum;
+    for(int i = 0; i < 6; i++){
+        cout << i + 1 << ": " << counts[i] << ", " << percentages[i] << " %" << endl;
+    }
     return 0;
 }
