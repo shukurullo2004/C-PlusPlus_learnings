908eafea347bff9467fd3657241b702b1bf97456
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-05-27 12:51:45 +0900

    rectified logic

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -2,48 +2,66 @@
 #include <string>
 
 using namespace std;
+
 struct data {
 	int year;
 	int month;
 	int day;
 };
+
 int main() {
 	struct data Student;
 	cout << "Enter your date of birth: " << endl;
 	cout << "\n-Year: ";
 	cin >> Student.year;
 	cout << "\n-Month: ";
-	cin >>Student.month;
+	cin >> Student.month;
 	cout << "\n-Day: ";
 	cin >> Student.day;
+
 	string mont;
 	switch (Student.month) {	
-	case 1 :
-		mont ="January,";
-	case 2:
-		mont = "February,";
-	case 3:
-		mont = "March,";
-	case 4:
-		mont = "April,";
-	case 5:
-		mont = "May,";
-	case 6:
-		mont = "June,";
-	case 7:
-		mont = "July";
-	case 8:
-		mont ="August,";
-	case 9:
-		mont = "September,";
-	case 10:
-		mont = "October,";
-	case 11:
-		mont = "November,";
-	case 12:
-		mont = "December,";
+		case 1:
+			mont = "January,";
+			break;
+		case 2:
+			mont = "February,";
+			break;
+		case 3:
+			mont = "March,";
+			break;
+		case 4:
+			mont = "April,";
+			break;
+		case 5:
+			mont = "May,";
+			break;
+		case 6:
+			mont = "June,";
+			break;
+		case 7:
+			mont = "July";
+			break;
+		case 8:
+			mont = "August,";
+			break;
+		case 9:
+			mont = "September,";
+			break;
+		case 10:
+			mont = "October,";
+			break;
+		case 11:
+			mont = "November,";
+			break;
+		case 12:
+			mont = "December,";
+			break;
+		default:
+			mont = "Invalid month,";
+			break;
 	}
-	std::cout << "Your DoB: " << mont << Student.day <<" " << Student.year << endl;
-	return 0;
 
+	cout << "Your DoB: " << mont << " " << Student.day << " " << Student.year << endl;
+	return 0;
 }
