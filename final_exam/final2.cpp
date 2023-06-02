a6fc6572b49d24abee76e92fead95bfa8d7726a4
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-06-03 01:28:24 +0900

    palindrome added

--- final_exam/final2.cpp
+++ final_exam/final2.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include <string>
 using namespace std;
+bool palindrome(string word);
 
-void main(){
-    
+int main(){
+    string word = "dad";
+    palindrome(word);
+
+    if(palindrome(word)){
+        cout <<"Yes\n";
+    }  
+    else
+        cout << "no\n";
+        return 0;
+}
+bool palindrome(string word){
+   int left = 0;
+   int right = sizeof(word)-1;
+   while(left < right){
+    if(word[left] != word[right]){
+        return false;
+    }
+   }
+    return true;
 }
